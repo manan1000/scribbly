06430043fc851f62d527c096bd4754c5af8d59ea
manan1000 <mananhemani29@gmail.com>
2025-01-22 18:25:25 +0530

    basic code

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,30 +1,33 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
+import { CreateUserSchema, SigninSchema, CreateRoomSchema } from "@repo/common/types";
 
 const app = express();
 
-app.post("/signup", function(req,res){
-    
+app.post("/signup", function (req, res) {
+
 
 });
 
-app.post("/signin", function(req,res){
-    const username="manan";
-    const password="manan";
+app.post("/signin", function (req, res) {
+    const username = "manan";
+    const password = "manan";   
+    const name = "manan";   
 
+    const data = CreateUserSchema.safeParse(req.body);
     const userId = 1;
 
-    const token = jwt.sign({userId},JWT_SECRET);
-    res.json({token});
+    const token = jwt.sign({ userId }, JWT_SECRET);
+    res.json({ token });
 
 });
 
 
-app.post("/room", middleware , function(req,res){
-    
-    res.json({roomId:})
+app.post("/room", middleware, function (req, res) {
+
+    res.json({ roomId: "1234"})
 });
 
 app.listen(5000);
